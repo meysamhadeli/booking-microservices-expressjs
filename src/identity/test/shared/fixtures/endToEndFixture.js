2e8a9d08753293b4417effb684b8e6fcc1116001
Pc <meysamhadeli@gmail.com>
2023-11-28 13:28:02 +0330

    chore: change ts-config structure in identity

--- src/identity/test/shared/fixtures/endToEndFixture.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EndToEndTestFixture = void 0;
-const integrationTestFixture_1 = require("./integrationTestFixture");
-class EndToEndTestFixture extends integrationTestFixture_1.IntegrationTestFixture {
-}
-exports.EndToEndTestFixture = EndToEndTestFixture;
-//# sourceMappingURL=endToEndFixture.js.map
