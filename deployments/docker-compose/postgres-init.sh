41536408fbe16714557dbb69d331ea27b04a7ca4
itachi5747 <nafaya209@gmail.com>
2026-07-21 15:04:15 +0500

    fix: move config to .env, use POSTGRES_DB, add .env.example

--- deployments/docker-compose/postgres-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-for db in identity flight passenger booking; do
-  if ! psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname = '$db'" | grep -q 1; then
-    createdb -U "$POSTGRES_USER" "$db"
-  fi
-done
